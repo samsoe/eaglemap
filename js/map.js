b7fc9b2308f3b9d2100a2c9753336f8c08f2f393
Samsoe <esamsoe@gmail.com>
2014-07-21 15:59:23 -0600

    Initial 1 Event Data Load

--- js/map.js
+++ js/map.js
@@ -34,14 +34,40 @@ $('li').on("click", function(){
 	$(this).toggleClass("active");
 });
 
-$('#initial-load').on("click", function() {
-	alert("Yo!");
-});
-
 /*** MOVEBANK ***/
 
+var loaded = false;
 var jsonUrl = "http://www.movebank.org/movebank/service/public/json";
 var study_id = 17707607;
 var individual_local_identifiers = [117188, 117410, 126401, 126402, 126403, 126404, 126405, 126406, 126407];
 var individual_local_names = ["117188", "117410", "126401", "126402", "126403", "126404", "126405", "126406", "126407"];
 var colors = ["purple", "red", "yellow", "blue", "green", "orange", "pink", "lightblue", "brown"];
+
+var timespan = 12; // months
+var date = new Date();
+var timestamp_end = date.getTime();
+var date_start = new Date();
+date_start = date_start.setMonth(date_start.getMonth() - timespan);
+var timestamp_start = date_start;
+
+var max_events_per_individual = 1;
+
+$('#initial-load').on("click", function() {
+	if(!loaded){
+		$.getJSON(jsonUrl + "?callback=?", {
+		  study_id: study_id,
+		  individual_local_identifiers: individual_local_identifiers,
+		  individual_local_names: individual_local_names,
+		  max_events_per_individual : max_events_per_individual,
+		  timestamp_start: timestamp_start,
+		  timestamp_end: timestamp_end,
+		  sensor_type: "gps"
+		}, function (data0) {
+		  data = data0;
+		  loaded = true;
+		  alert("Full Runthrough");
+		});
+	} else {
+		alert('Movebank Data Loaded.')
+	}
+});
