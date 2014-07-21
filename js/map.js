2bab09c7cce4ca1b86c52748468f0cd100a38035
Samsoe <esamsoe@gmail.com>
2014-07-21 15:39:59 -0600

    Panel Style Update

--- js/map.js
+++ js/map.js
@@ -29,3 +29,19 @@ function initialize() {
 }
 
 google.maps.event.addDomListener(window, 'load', initialize);
+
+$('li').on("click", function(){
+	$(this).toggleClass("active");
+});
+
+$('#initial-load').on("click", function() {
+	alert("Yo!");
+});
+
+/*** MOVEBANK ***/
+
+var jsonUrl = "http://www.movebank.org/movebank/service/public/json";
+var study_id = 17707607;
+var individual_local_identifiers = [117188, 117410, 126401, 126402, 126403, 126404, 126405, 126406, 126407];
+var individual_local_names = ["117188", "117410", "126401", "126402", "126403", "126404", "126405", "126406", "126407"];
+var colors = ["purple", "red", "yellow", "blue", "green", "orange", "pink", "lightblue", "brown"];
