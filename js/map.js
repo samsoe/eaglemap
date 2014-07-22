37b4e24a28e86fe9fc80a92ee591210a401a740c
Samsoe <esamsoe@gmail.com>
2014-07-22 17:11:55 -0600

    1 Event Data Load Working

--- js/map.js
+++ js/map.js
@@ -1,9 +1,13 @@
-/*** eagle map w/ git ***/
+
+/*** 
+ ::: Eagle Map w/ git 
+ ::: Active Development 07/22 mailto:esamsoe@gmail.com 
+***/
 
 var map;
-var data = [];
+var data;
 
-function initialize() {
+$(document).ready(function($) {
 	var mapOptions = {
 		zoom: 9,
 		center: new google.maps.LatLng(46.692, -114.015),
@@ -30,24 +34,19 @@ function initialize() {
 	};
 
 	map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
-}
-
-google.maps.event.addDomListener(window, 'load', initialize);
-
-$('li').on("click", function(){
-	$(this).toggleClass("active");
 });
+
 
 /*** MOVEBANK ***/
 
-var loaded = false;
+
 var jsonUrl = "http://www.movebank.org/movebank/service/public/json";
 var study_id = 17707607;
 var individual_local_identifiers = [117188, 117410, 126401, 126402, 126403, 126404, 126405, 126406, 126407];
 var individual_local_names = ["117188", "117410", "126401", "126402", "126403", "126404", "126405", "126406", "126407"];
 var colors = ["purple", "red", "yellow", "blue", "green", "orange", "pink", "lightblue", "brown"];
 
-var timespan = 12; // months
+var timespan = 2; // months
 var date = new Date();
 var timestamp_end = date.getTime();
 var date_start = new Date();
@@ -57,21 +56,305 @@ var timestamp_start = date_start;
 var max_events_per_individual = 1;
 
 $('#initial-load').on("click", function() {
-	if(!loaded){
-		$.getJSON(jsonUrl + "?callback=?", {
-		  study_id: study_id,
-		  individual_local_identifiers: individual_local_identifiers,
-		  individual_local_names: individual_local_names,
-		  max_events_per_individual : max_events_per_individual,
-		  timestamp_start: timestamp_start,
-		  timestamp_end: timestamp_end,
-		  sensor_type: "gps"
-		}, function (data0) {
-		  data = data0;
-		  loaded = true;
-		  alert("Movebank Data Load Function Complete");
-		});
-	} else {
-		alert('Movebank Data Loaded.')
+	$.getJSON(jsonUrl + "?callback=?", {
+	  study_id: study_id,
+	  individual_local_identifiers: individual_local_identifiers,
+	  individual_local_names: individual_local_names,
+	  max_events_per_individual : max_events_per_individual,
+	  timestamp_start: timestamp_start,
+	  timestamp_end: timestamp_end,
+	  sensor_type: "gps"
+}, function (data0) {
+	data = data0;
+	$('#initial-load').css("display","none");
+
+	/* more loading */
+	for (i = 0; i < data.individuals.length; i++) {
+		data.individuals[i].color = colors[i];
+		//data.individuals[i].name = name[i]; // no eagle names so far
 	}
+
+	createMarkers();
+	createPolylines();
+	showCurrent();                
+
+	startDate = null;
+	endDate = null;
+	for (i = 0; i < data.individuals.length; i++) {
+	    for (j = 0; j < data.individuals[i].locations.length; j++) {
+	        ts = data.individuals[i].locations[j].timestamp;
+	        if (startDate != null) {
+	            startDate = Math.min(startDate, ts);
+	            endDate = Math.max(endDate, ts);
+	        } else {
+	            startDate = ts;
+	            endDate = ts;
+	        }
+	    }
+	}
+	for (i = 0; i < data.individuals.length; i++)
+	    showClosestPointInTime(data.individuals[i], endDate);
+
+	});
+});
+
+function createMarkers() {
+    for (i = 0; i < data.individuals.length; i++) {
+        data.individuals[i].marker = new google.maps.Marker({
+            clickable: true,
+            draggable: true,
+            icon: {
+                path: google.maps.SymbolPath.CIRCLE,
+                fillOpacity: 0.7,
+                fillColor: data.individuals[i].color,
+                strokeWeight: 1,
+                strokeOpacity: 0.5,
+                strokeColor: 'white',
+                scale: 6
+            },
+            optimized: true
+        });
+
+        data.individuals[i].marker.setPosition(new google.maps.LatLng(null, null));
+        data.individuals[i].marker.setTitle(data.individuals[i].individual_local_identifier);
+        google.maps.event.addListener(data.individuals[i].marker, "click", (function (individual) {
+            return function () {
+                showInfo(individual);
+            };
+        })(data.individuals[i]));
+        //data.individuals[i].marker.setVisible(false);
+    }
+    setBounds();
+}
+
+function setBounds() {
+    var bounds = new google.maps.LatLngBounds();
+    for (i = 0; i < data.individuals.length; i++) {
+        for (j = 0; j < data.individuals[i].locations.length; j++) {
+            bounds.extend(new google.maps.LatLng(
+                data.individuals[i].locations[j].location_lat,
+                data.individuals[i].locations[j].location_long));
+        }
+    }
+    map.fitBounds(bounds);
+}
+
+function createPolylines() {
+    for (i = 0; i < data.individuals.length; i++) {
+        var track = [];
+        for (j = 0; j < data.individuals[i].locations.length; j++) {
+            track[j] = new google.maps.LatLng(
+                data.individuals[i].locations[j].location_lat,
+                data.individuals[i].locations[j].location_long);
+        }
+        icons = [{
+                icon: {
+                    path: google.maps.SymbolPath.FORWARD_CLOSED_ARROW
+                },
+                offset: 0,
+                repeat: '100px'
+            }
+        ];
+        data.individuals[i].polyline = new google.maps.Polyline({
+            path: track,
+            clickable: true,
+            strokeColor: data.individuals[i].color,
+            strokeOpacity: 0.5,
+            strokeWeight: 3
+            //icons : icons
+        });
+        //data.individuals[i].polyline.setMap(null);
+        google.maps.event.addListener(data.individuals[i].polyline, 'click', (function (
+            individual) {
+            return function (e) {
+                showClosestPointInSpace(individual, e.latLng, true);
+                showInfo(individual);
+            };
+        })(data.individuals[i]));
+
+        console.log(data.individuals[i].locations.length);
+    }
+}
+
+function showCurrent() {
+    // data.individuals[i].marker.setVisible(true);
+    for (i = 0; i < data.individuals.length; i++) {
+        data.individuals[i].marker.setVisible(true);
+        data.individuals[i].polyline.setMap(map);
+    }
+}
+
+function showClosestPointInTime(individual, t) {
+    var distCurr = 1000 * 1000 * 1000 * 1000;
+    var indexCurr;
+    for (j = 0; j < individual.locations.length; j++) {
+        dist0 = Math.abs(t - individual.locations[j].timestamp);
+        if (dist0 < distCurr) {
+            indexCurr = j;
+            distCurr = dist0;
+        }
+    }
+    if (indexCurr == 0)
+        indexStart = 1;
+    else
+        indexStart = indexCurr - 1;
+    if (indexCurr == individual.locations.length - 1)
+        indexEnd = individual.locations.length - 2;
+    else
+        indexEnd = indexCurr + 1;
+    indexClosest = indexCurr;
+    distClosest = distCurr;
+    distCurr = 1000 * 1000 * 1000;
+    for (j = indexStart; j <= indexEnd; j += 2) {
+        dist0 = Math.abs(t - individual.locations[j].timestamp);
+        if (dist0 < distCurr) {
+            indexCurr = j;
+            distCurr = dist0;
+        }
+    }
+    indexSecondClosest = indexCurr;
+    distSecondClosest = distCurr;
+    x0 = individual.locations[indexClosest].location_long;
+    y0 = individual.locations[indexClosest].location_lat;
+    x1 = individual.locations[indexSecondClosest].location_long;
+    y1 = individual.locations[indexSecondClosest].location_lat;
+    x = (x0 * distSecondClosest + x1 * distClosest) / (distClosest + distSecondClosest);
+    y = (y0 * distSecondClosest + y1 * distClosest) / (distClosest + distSecondClosest);
+    individual.marker.setPosition(new google.maps.LatLng(y, x));
+    individual.marker.timestamp = t;
+    if (individual.marker.getMap() == null)
+        individual.marker.setMap(map);
+    gracePeriod = 1000 * 60 * 60 * 24 * 2;
+    if (t + gracePeriod < individual.locations[0].timestamp || t - gracePeriod > individual.locations[individual.locations.length - 1].timestamp)
+        individual.marker.setMap(null);
+}
+
+function showInfo(individual) {
+    wasOpen = (individual.info != null);
+    hideInfos();
+    if (!wasOpen && individual.marker) {
+        individual.info = new google.maps.InfoWindow();
+        updateInfo(individual);
+        individual.info.open(map, individual.marker);
+    }
+}
+
+function hideInfos() {
+    for (i = 0; i < data.individuals.length; i++) {
+        if (data.individuals[i].info) {
+            data.individuals[i].info.close();
+            data.individuals[i].info = null;
+        }
+    }
+}
+
+function updateInfo(individual) {
+    if (individual.info && individual.marker) {
+        tsFirst = individual.locations[0].timestamp;
+        tsLast = individual.locations[individual.locations.length - 1].timestamp;
+        ts = individual.marker.timestamp;
+        if (ts < tsFirst)
+            ts = tsFirst;
+        if (ts > tsLast)
+            ts = tsLast;
+        individual.info.setContent("<div style='width:101px;height:40px;'><b>" + individual.name + "</b>" + "<br>" + "Date: " + formatTimestamp(ts) + "</div>");
+    }
+}
+
+function showClosestPointInSpace(individual, latLng, snapToPoint) {
+    var distCurr = 1000 * 1000 * 1000 * 1000;
+    var indexCurr;
+    for (j = 0; j < individual.locations.length; j++) {
+        latLng0 = new google.maps.LatLng(
+            individual.locations[j].location_lat,
+            individual.locations[j].location_long);
+        dist0 = google.maps.geometry.spherical.computeDistanceBetween(
+            latLng, latLng0);
+        if (dist0 < distCurr) {
+            indexCurr = j;
+            distCurr = dist0;
+        }
+    }
+    if (indexCurr == 0)
+        indexStart = 1;
+    else
+        indexStart = indexCurr - 1;
+    if (indexCurr == individual.locations.length - 1)
+        indexEnd = individual.locations.length - 2;
+    else
+        indexEnd = indexCurr + 1;
+    indexClosest = indexCurr;
+    distCurr = 1000 * 1000 * 1000 * 1000;
+    for (j = indexStart; j <= indexEnd; j += 2) {
+        latLng0 = new google.maps.LatLng(
+            individual.locations[j].location_lat,
+            individual.locations[j].location_long);
+        dist0 = google.maps.geometry.spherical.computeDistanceBetween(
+            latLng, latLng0);
+        if (dist0 < distCurr) {
+            indexCurr = j;
+            distCurr = dist0;
+        }
+    }
+    indexSecondClosest = indexCurr;
+    if (snapToPoint)
+        indexSecondClosest = indexClosest;
+    pointOnLine = getPointClosestToLine(
+        individual.locations[indexClosest].location_long,
+        individual.locations[indexClosest].location_lat,
+        individual.locations[indexSecondClosest].location_long,
+        individual.locations[indexSecondClosest].location_lat,
+        latLng.lng(), latLng.lat());
+    individual.marker.setPosition(new google.maps.LatLng(pointOnLine.y,
+        pointOnLine.x));
+    if (individual.marker.getMap() == null)
+        individual.marker.setMap(map);
+    latLngClosest = new google.maps.LatLng(
+        individual.locations[indexClosest].location_lat,
+        individual.locations[indexClosest].location_long);
+    distClosest = google.maps.geometry.spherical
+        .computeDistanceBetween(latLng, latLngClosest);
+    latLngSecondClosest = new google.maps.LatLng(
+        individual.locations[indexSecondClosest].location_lat,
+        individual.locations[indexSecondClosest].location_long);
+    distSecondClosest = google.maps.geometry.spherical
+        .computeDistanceBetween(latLng, latLngSecondClosest);
+    t = (individual.locations[indexClosest].timestamp * distSecondClosest + individual.locations[indexSecondClosest].timestamp * distClosest) / (distClosest + distSecondClosest);
+    individual.marker.timestamp = t;
+    for (i = 0; i < data.individuals.length; i++)
+        if (data.individuals[i] != individual)
+            showClosestPointInTime(data.individuals[i], t);
+    $('#time-display').datepicker('setDate', new Date(t));
+}
+
+function formatTimestamp(timestamp) {
+    var date = new Date(timestamp);
+    var ss = date.getSeconds();
+    var mi = date.getMinutes();
+    var hh = date.getHours();
+    var dd = date.getDate();
+    var mm = date.getMonth() + 1;
+    var yyyy = date.getFullYear();
+    if (ss < 10) {
+        ss = '0' + ss;
+    }
+    if (mi < 10) {
+        mi = '0' + mi;
+    }
+    if (hh < 10) {
+        hh = '0' + hh;
+    }
+    if (dd < 10) {
+        dd = '0' + dd;
+    }
+    if (mm < 10) {
+        mm = '0' + mm;
+    }
+    return mm + "-" + dd + "-" + yyyy;
+}
+
+
+/*** Buttons ***/
+$('li').on("click", function(){
+	$(this).toggleClass("active");
 });
